637098291641f2492d829d29125a5d3c71c832ea
okkogael <okkogael@gmail.com>
2024-05-25 13:47:34 -0300

    main

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBtn = document.querySelector('.menu-btn');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuBtn.addEventListener('click', function() {
+        navLinks.classList.toggle('show');
+    });
+});
