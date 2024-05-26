ee9f450b2599820fbfea93772474adf654813deb
okkogael <okkogael@gmail.com>
2024-05-25 21:33:13 -0300

    main

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuBtn = document.querySelector('.menu-btn');
-    const navLinks = document.querySelector('.nav-links');
-
-    menuBtn.addEventListener('click', function() {
-        navLinks.classList.toggle('show');
-    });
-});
